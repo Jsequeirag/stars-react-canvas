601f1c458ab1638f61c6e4f227f2d792ff0586df
Jose Luis <joseluissequeirag@gmail.com>
2022-03-26 15:42:34 -0600

    library react-ztext added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import Ztext from "react-ztext";
 import "./home.css";
 export default function Home() {
   return (
     <div className="Home">
-      <h1>Jose Luis Sequeira</h1>
+      <Ztext
+        depth="1rem"
+        direction="both"
+        event="pointer"
+        eventRotation="30deg"
+        eventDirection="default"
+        fade={false}
+        layers={10}
+        perspective="500px"
+        style={{
+          fontSize: "4rem",
+        }}
+      >
+        <span role="img" aria-label="emoji">
+          Jose Luis Sequeira
+        </span>
+      </Ztext>
     </div>
   );
 }
